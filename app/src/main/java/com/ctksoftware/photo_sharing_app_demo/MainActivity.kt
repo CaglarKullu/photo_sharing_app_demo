c1ccaf30fa687f07c1f978ee4334854d7edd00e5
Caglar Kullu <caglarkullu@gmail.com>
2023-04-06 17:11:59 +0100

    fix:gradle

--- app/src/main/java/com/ctksoftware/photo_sharing_app_demo/MainActivity.kt
+++ app/src/main/java/com/ctksoftware/photo_sharing_app_demo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.ctksoftware.photo_sharing_app_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun logIn(view: View){
+
+    }
+
+    fun singUp(view: View){
+
     }
 }
